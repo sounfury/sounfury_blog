77efe28f6a879bc94c119acb64e59c371276b8a6
light <a2133266@outlook.com>
2024-09-09 00:11:09 +0800

    init

---
+++ src/main/java/com/sounfury/sounfury_blog/SounfuryBlogApplication.java
@@ -0,0 +1,13 @@
+package com.sounfury.sounfury_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SounfuryBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SounfuryBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sounfury/sounfury_blog/SounfuryBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sounfury.sounfury_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SounfuryBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
